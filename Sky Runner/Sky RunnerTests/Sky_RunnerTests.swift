7c385eb481540575592cc83655befce8fd93b683
Manny <tenoriom@lafayette.edu>
2017-08-11 19:36:28 -0500

    Starting Sky Runner

---
+++ Sky Runner/Sky RunnerTests/Sky_RunnerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sky_RunnerTests.swift
+//  Sky RunnerTests
+//
+//  Created by Manuel Tenorio on 8/11/17.
+//  Copyright © 2017 Manuel Tenorio. All rights reserved.
+//
+
+import XCTest
+@testable import Sky_Runner
+
+class Sky_RunnerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
